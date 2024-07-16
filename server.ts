d0b9e0ea812bc2a8b1f2ef5265fdbe3aebf407c8
Ben F. M. Williams <8977479+psalm119@users.noreply.github.com>
2024-07-15 22:35:46 -0500

    ex20

--- server.ts
+++ server.ts
@@ -4,71 +4,53 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
-import * as dotenv from 'dotenv';
-import { DatabaseService } from './src/app/database.service';
-
-dotenv.config({ path: '.env.development.local' }); // Load environment variables
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
-  const commonEngine = new CommonEngine();
+    const server = express();
+    const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+    const browserDistFolder = resolve(serverDistFolder, '../browser');
+    const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
+    const commonEngine = new CommonEngine();
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
+    server.set('view engine', 'html');
+    server.set('views', browserDistFolder);
 
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+    // Example Express Rest API endpoints
+    // server.get('/api/**', (req, res) => { });
+    // Serve static files from /browser
+    server.get('*.*', express.static(browserDistFolder, {
+        maxAge: '1y'
+    }));
 
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
+    // All regular routes use the Angular engine
+    server.get('*', (req, res, next) => {
+        const { protocol, originalUrl, baseUrl, headers } = req;
 
+        commonEngine
+            .render({
+                bootstrap,
+                documentFilePath: indexHtml,
+                url: `${protocol}://${headers.host}${originalUrl}`,
+                publicPath: browserDistFolder,
+                providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+            })
+            .then((html) => res.send(html))
+            .catch((err) => next(err));
+    });
 
-  // Initialize the database service
-  const databaseService = new DatabaseService();
-
-  // Example API endpoint to fetch data
-  server.get('/api/tbl1', async (req, res) => {
-    try {
-      const data = await databaseService.getAllFromTbl1();
-      res.json(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-
-  return server;
+    return server;
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+    const port = process.env['PORT'] || 4000;
 
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+    // Start up the Node server
+    const server = app();
+    server.listen(port, () => {
+        console.log(`Node Express server listening on http://localhost:${port}`);
+    });
 }
 
 run();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-// import { DatabaseService } from './database.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 't1';
-  tbl1Data: any[] | undefined;
-
-  // constructor(private http: HttpClient, private databaseService: DatabaseService) { }
-  constructor(private http: HttpClient) { }
-
-  // ngOnInit() {
-  //   console.log('AppComponent ngOnInit');
-
-  //   // this.http.get('/api/tbl1').subscribe(data => {
-  //   //   console.log('data', data);
-  //   //   this.tbl1Data = data as any[];
-  //   // }
-  //   // );
-
-  // }
-  async ngOnInit() {
-    try {
-      // this.tbl1Data = await this.databaseService.query('SELECT * FROM tbl1');
-      // console.log('Data from database:', this.tbl1Data);
-    } catch (err) {
-      console.error('Error fetching data:', err);
-    }
-  }
 }
